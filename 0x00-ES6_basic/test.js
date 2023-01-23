76e3c0a9ba5a01d91b2b83d64ca54a1da1e8dc60
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-23 21:21:31 +0300

    Task 1

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,4 +1,12 @@
 // Task 0 test case
-import { taskFirst, taskNext } from './0-constants.js';
+// import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+// console.log(`${taskFirst()} ${taskNext()}`);
+
+
+// Task 1 test Case
+
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
