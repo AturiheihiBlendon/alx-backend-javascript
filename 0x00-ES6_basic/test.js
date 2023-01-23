8316d73bf0904d1a9abd68626214703e51c47f39
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-23 21:52:00 +0300

    Task 3

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -12,8 +12,15 @@
 
 // Task 2 test case
 
-import getNeighborhoodsList from './2-arrow.js';
+// import getNeighborhoodsList from './2-arrow.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+// const neighborhoodsList = new getNeighborhoodsList();
+// const res = neighborhoodsList.addNeighborhood('Noe Valley');
+// console.log(res);
+
+// Task 2 test case
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
