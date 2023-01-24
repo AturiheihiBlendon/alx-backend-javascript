e23315cee00886375cb0edb55a4b0817e3582d67
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-24 22:48:54 +0300

    Task 2

--- 0x01-ES6_promise/test.js
+++ 0x01-ES6_promise/test.js
@@ -4,8 +4,14 @@
 // const response = getResponseFromAPI();
 // console.log(response instanceof Promise);
 
-// Task 1 test case
-import getFullResponseFromAPI from './1-promise';
+// // Task 1 test case
+// import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+// console.log(getFullResponseFromAPI(true));
+// console.log(getFullResponseFromAPI(false));
+
+// Task2 test case
+import handleResponseFromAPI from "./2-then";
+
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
