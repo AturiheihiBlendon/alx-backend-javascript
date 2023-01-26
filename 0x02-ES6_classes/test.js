89d47c826fbfd550719235ab0973c27ca02b8ab7
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-26 17:06:50 +0300

    Task 5

--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -40,9 +40,25 @@
 
 // Task 4 test case
 
-import Pricing from './4-pricing.js';
-import Currency from './3-currency.js';
+// import Pricing from './4-pricing.js';
+// import Currency from './3-currency.js';
 
-const p = new Pricing(100, new Currency("EUR", "Euro"))
-console.log(p);
-console.log(p.displayFullPrice());
+// const p = new Pricing(100, new Currency("EUR", "Euro"))
+// console.log(p);
+// console.log(p.displayFullPrice());
+
+// Task 5 test case
+
+import Building from './5-building.js';
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+    new TestBuilding(200)
+}
+catch(err) {
+    console.log(err);
+}
