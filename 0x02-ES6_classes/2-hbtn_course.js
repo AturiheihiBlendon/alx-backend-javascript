cccfc9477317b2d7ff018c93d2dbdb651b8c45bf
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-26 14:46:10 +0300

    Fixed error msg

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -13,7 +13,7 @@ export default class HolbertonCourse {
   // name setter
   set name(val) {
     if (typeof (val) !== 'string') {
-      throw new TypeError('Name should be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = val;
   }
@@ -26,7 +26,7 @@ export default class HolbertonCourse {
   // length setter
   set length(val) {
     if (typeof (val) !== 'number') {
-      throw new TypeError('Length should be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = val;
   }
@@ -40,7 +40,7 @@ export default class HolbertonCourse {
   set students(val) {
     val.forEach((student) => {
       if (typeof student !== 'string') {
-        throw new TypeError('Students should be an array of strings');
+        throw new TypeError('Students must be an array of strings');
       }
     });
     this._students = val;
