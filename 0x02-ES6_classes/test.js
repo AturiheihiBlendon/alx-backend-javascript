cc85423b4ffd5aad18ff8226ed633caedd0c4d5d
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-26 14:21:52 +0300

    Task 1

--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -1,5 +1,10 @@
 // Task 0 test case
-import ClassRoom from "./0-classroom.js";
+// import ClassRoom from "./0-classroom.js";
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+// const room = new ClassRoom(10);
+// console.log(room._maxStudentsSize)
+
+// Task 1 test case
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
