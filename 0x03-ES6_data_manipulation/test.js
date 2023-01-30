53c49c08be88aa0c28a3830602b93728f85656bd
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-30 11:07:55 +0300

    Task 3

--- 0x03-ES6_data_manipulation/test.js
+++ 0x03-ES6_data_manipulation/test.js
@@ -11,9 +11,18 @@
 // console.log(getListStudentIds(getListStudents()));
 
 // Task 2 test case
+// import getListStudents from "./0-get_list_students.js";
+// import getStudentsByLocation from "./2-get_students_by_loc.js";
+
+// const students = getListStudents();
+
+// console.log(getStudentsByLocation(students, 'San Francisco'));
+
+// Task 3 test case
 import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
+import getStudentIdsSum from "./3-get_ids_sum.js";
 
 const students = getListStudents();
+const value = getStudentIdsSum(students);
 
-console.log(getStudentsByLocation(students, 'San Francisco'));
+console.log(value);
