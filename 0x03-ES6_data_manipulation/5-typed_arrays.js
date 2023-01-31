7c2c1df321f41c52390e829c7b2a56b2fd879dba
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-31 15:46:10 +0300

    Task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer);
+
+  // check if position outside range
+  if (position >= length) {
+    throw new Error('Position outside range');
+  } else {
+    dv.setInt8(position, value);
+  }
+
+  return dv;
+}
