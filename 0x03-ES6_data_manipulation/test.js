ff46ad6219bc393495f56c4bd53e65e39be3053a
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-31 15:47:59 +0300

    Task 7

--- 0x03-ES6_data_manipulation/test.js
+++ 0x03-ES6_data_manipulation/test.js
@@ -37,6 +37,11 @@
 
 // Task  5 test case
 
-import createInt8TypedArray from "./5-typed_arrays.js";
+// import createInt8TypedArray from "./5-typed_arrays.js";
 
-console.log(createInt8TypedArray(10, 2, 89));
+// console.log(createInt8TypedArray(10, 2, 89));
+
+// Task 6 test case
+import setFromArray from "./6-set.js";
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
