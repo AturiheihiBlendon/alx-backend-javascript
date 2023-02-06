a8d8532386a43833b42961d13154730091549508
AturiheihiBledon <2020blendon@gmail.com>
2023-02-06 17:26:38 +0300

    Task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+//Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any
+}
