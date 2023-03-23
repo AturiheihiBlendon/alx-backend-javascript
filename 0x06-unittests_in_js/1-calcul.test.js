4088de83b686fbb2a8bdec6f51a03e84ba74d765
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-23 10:43:54 +0300

    task 1 -Test with mocha assertion error

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,28 @@
+const assert  = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('Test advanced calculateNumber()', function() {
+    describe('Tests SUM ():', function(){
+        it('returns 6', function() {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
+        })
+    })
+
+    describe('Tests SUBTRACT ():', function(){
+        it('return -4', function() {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+        })
+    })
+
+    describe('Tests DIVIVDE ():', function(){
+        it('return 0.2', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
+        })
+    })
+
+    describe('Tests DIVIVDE () with 0:', function(){
+        it('return Error', function() {
+            assert(calculateNumber('DIVIDE', 1.4, 0), 'Error')
+        })
+    })
+})
