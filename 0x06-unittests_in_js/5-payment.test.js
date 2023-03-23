fc5bc33951e643d4ab56fcef725c1a0e058e2651
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-23 19:49:52 +0300

    hooks in unittests

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const { expect } = require('chai')
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log The total is: 120 to the console once', () => {
+    sendPaymentRequestToApi(100, 20);
+    // assert(consoleLogSpy.calledOnceWith('The total is: 120'));
+    expect(consoleLogSpy.calledOnceWith('The total is: 120'));
+  });
+
+  it('should log The total is: 20 to the console once', () => {
+    sendPaymentRequestToApi(10, 10);
+    // assert(consoleLogSpy.calledOnceWith('The total is: 20'));
+    expect(consoleLogSpy.calledOnceWith('The total is: 20'));
+  });
+});
