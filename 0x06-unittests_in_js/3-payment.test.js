845454999dbac7e2205e245471eff8a5f2727944
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-23 19:16:15 +0300

    Spying in unittests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const assert = require('assert');
+const { expect } = require('chai')
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
@@ -8,7 +9,8 @@ describe('sendPaymentRequestToApi', function() {
     const sinonSpy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
 
-    assert(sinonSpy.calledWithExactly('SUM', 100, 20));
+    // assert(sinonSpy.calledWithExactly('SUM', 100, 20));
+    expect(sinonSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     sinonSpy.restore();
   });
 });
